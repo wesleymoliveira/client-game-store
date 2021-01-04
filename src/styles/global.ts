011249e186c4ad58b70ea023f6452126676c6163
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-01-04 15:23:03 -0300

    add fonts theme provider and types theme

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,21 +1,52 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, css } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+
+@font-face {
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 300;
+  font-display: swap;
+  src: local(''),
+       url('/fonts/poppins-v15-latin-300.woff2') format('woff2');
+}
+
+@font-face {
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: local(''),
+       url('/fonts/poppins-v15-latin-regular.woff2') format('woff2');
+
+}
+
+@font-face {
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 600;
+  font-display: swap;
+  src: local(''),
+       url('/fonts/poppins-v15-latin-600.woff2') format('woff2');
+}
   * {
     margin: 0;
     padding:0;
     box-sizing: border-box;
-  }
-  html {
-    font-size: 62.5%;
-  }
-  html, body, #__next {
-    height: 100%;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
-  body {
-    font-family: Open-Sans, Helvetica, Sans-Serif, Roboto;
-  }
+  ${({ theme }) => css`
+    html {
+      font-size: 62.5%;
+    }
+    body {
+      font-family: ${theme.font.family};
+      font-size: ${theme.font.sizes.medium};
+    }
+  `}
+
 `
 
 export default GlobalStyles
