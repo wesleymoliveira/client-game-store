062e191f7074384dd45e44166584e56e96c4036a
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-01-05 18:33:36 -0300

    Add Component Menu Mobile - without styles

---
+++ src/components/Menu/test.tsx
@@ -0,0 +1,37 @@
+import { fireEvent, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Menu from '.'
+
+describe('<Menu />', () => {
+  it('should render the Menu', () => {
+    renderWithTheme(<Menu />)
+
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
+    expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
+  })
+
+  it('should render open/close mobile Menu', () => {
+    renderWithTheme(<Menu />)
+
+    // selecionar o menu FullScreen
+    const fullMenuElement = screen.getByRole('navigation', { hidden: true })
+    // Verificar se o Menu está escondido (hideonmobile)
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+
+    //clicar e verificar se ele abriu
+
+    fireEvent.click(screen.getByLabelText(/open menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
+    expect(fullMenuElement).toHaveStyle({ opacity: 1 })
+
+    //clicar e verificar se ele fechou
+
+    fireEvent.click(screen.getByLabelText(/close menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+  })
+})
