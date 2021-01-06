1aa4ff47d1f0b681e1eeb43bfaa0489d8f25fbb0
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-01-06 00:16:49 -0300

    Change Button component to be polimorphic "as" a button or a link

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,19 @@
+import { AnchorHTMLAttributes, ButtonHTMLAttributes } from 'react'
 import * as S from './styles'
 
+type ButtonTypes =
+  | AnchorHTMLAttributes<HTMLAnchorElement>
+  | ButtonHTMLAttributes<HTMLAnchorElement>
+
 export type ButtonProps = {
-  children?: React.ReactNode
+  //children?: React.ReactNode
   size?: 'small' | 'medium' | 'large'
   fullWidth?: boolean
   icon?: JSX.Element
-  onClick?: () => (event: React.MouseEvent<HTMLButtonElement>) => void
-}
+  as?: React.ElementType
+  //forma bem estrita de tipar...(adicionando osegundo type posso usar o botão como botão ou link)
+  // onClick?: () => (event: React.MouseEvent<HTMLButtonElement>) => void
+} & ButtonTypes
 const Button = ({
   icon,
   children,
