83f98f2309c23b676d42c43a827696ea377b8061
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-01-07 01:11:57 -0300

    change component banner to accept  Ribbon

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -4,31 +4,47 @@ import { renderWithTheme } from 'utils/tests/helpers'
 import Banner from '.'
 
 const props = {
-  img: 'https:source.unsplash.com/user/willianjusten/1042X580',
+  img: 'https://source.unsplash.com/user/willianjusten/1042x580',
   title: 'Defy death',
-  subtitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season',
   buttonLabel: 'Buy now',
   buttonLink: '/games/defy-death',
 }
 
 describe('<Banner />', () => {
-  it('should render the heading', () => {
+  it('should render correctly', () => {
     const { container } = renderWithTheme(<Banner {...props} />)
 
     expect(
-      screen.getAllByRole('heading', { name: /Defy death/i }),
+      screen.getByRole('heading', { name: /Defy death/i }),
     ).toBeInTheDocument()
 
     expect(
-      screen.getAllByRole('heading', {
-        name: /Play the new crashLands season/i,
-      }),
+      screen.getByRole('heading', { name: /Play the new CrashLands season/i }),
     ).toBeInTheDocument()
 
-    expect(
-      screen.getAllByRole('img', { name: /Defy death/i }),
-    ).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /Defy death/i })).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render a Ribbon', () => {
+    renderWithTheme(
+      <Banner
+        {...props}
+        ribbon="My Ribbon"
+        ribbonSize="small"
+        ribbonColor="secondary"
+      />,
+    )
+
+    const ribbon = screen.getByText(/My Ribbon/i)
+
+    expect(ribbon).toBeInTheDocument()
+    expect(ribbon).toHaveStyle({ backgroundColor: '#3CD3C1' })
+    expect(ribbon).toHaveStyle({
+      height: '2.6rem',
+      fontSize: '1.2rem',
+    })
   })
 })
