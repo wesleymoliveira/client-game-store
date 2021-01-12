f48516f7481cd880257499342c563233ef7b5864
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-01-12 16:07:04 -0300

    add component GameCard with Ribbon

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -2,32 +2,64 @@ import * as S from './styles'
 import {
   FavoriteBorder,
   AddShoppingCart,
+  Favorite,
 } from '@styled-icons/material-outlined'
 import Button from 'components/Button'
+import Ribbon, { RibbonSizes, RibbonColors } from 'components/Ribbon'
 
 export type GameCardProps = {
   title: string
   developer: string
   img: string
   price: string
+  promotionalPrice?: string
+  favorite?: boolean
+  ribbon?: React.ReactNode
+  ribbonColor?: RibbonColors
+  ribbonSize?: RibbonSizes
+  onFav?: () => void
 }
 
-const GameCard = ({ developer, title, img, price }: GameCardProps) => (
+const GameCard = ({
+  developer,
+  title,
+  img,
+  price,
+  promotionalPrice,
+  favorite = false,
+  ribbon,
+  ribbonColor = 'primary',
+  ribbonSize = 'small',
+  onFav,
+}: GameCardProps) => (
   <S.Wrapper>
+    {!!ribbon && (
+      <Ribbon color={ribbonColor} size={ribbonSize}>
+        {ribbon}
+      </Ribbon>
+    )}
+
     <S.ImageBox>
       <img src={img} alt={title} />
     </S.ImageBox>
-    <S.Info>
-      <S.Title>{title}</S.Title>
-      <S.Developer>{developer}</S.Developer>
-    </S.Info>
-    <S.FavButton role="button">
-      <FavoriteBorder aria-label="Add to wish List" />
-    </S.FavButton>
-    <S.BuyBox>
-      <S.Price>{price}</S.Price>
-      <Button icon={<AddShoppingCart />} size="small" />
-    </S.BuyBox>
+    <S.Content>
+      <S.Info>
+        <S.Title>{title}</S.Title>
+        <S.Developer>{developer}</S.Developer>
+      </S.Info>
+      <S.FavButton onClick={onFav} role="button">
+        {favorite ? (
+          <Favorite aria-label="Remove from wishList" />
+        ) : (
+          <FavoriteBorder aria-label="Add to wishList" />
+        )}
+      </S.FavButton>
+      <S.BuyBox>
+        {!!promotionalPrice && <S.Price isPromotional>{price}</S.Price>}
+        <S.Price>{promotionalPrice || price}</S.Price>
+        <Button icon={<AddShoppingCart />} size="small" />
+      </S.BuyBox>
+    </S.Content>
   </S.Wrapper>
 )
 
--- src/components/GameCard/stories.tsx
+++ src/components/GameCard/stories.tsx
@@ -9,7 +9,28 @@ export default {
     developer: 'Rockstar Games',
     img: 'https://source.unsplash.com/user/willianjusten/300X140',
     price: 'R$ 235,00',
+    promotionalPrice: 'R$ 200,00',
+  },
+  argTypes: {
+    onFav: { action: 'clicked' },
+    ribbon: { type: 'string' },
   },
 } as Meta
 
-export const Default: Story<GameCardProps> = (args) => <GameCard {...args} />
+export const Default: Story<GameCardProps> = (args) => (
+  <div style={{ width: '30rem' }}>
+    <GameCard {...args} />
+  </div>
+)
+
+export const WithRibbon: Story<GameCardProps> = (args) => (
+  <div style={{ width: '30rem' }}>
+    <GameCard {...args} />
+  </div>
+)
+
+WithRibbon.args = {
+  ribbon: '20% OFF',
+  ribbonSize: 'small',
+  ribbonColor: 'primary',
+}
