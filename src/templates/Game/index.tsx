d88bed1abd6f33577dde8944c63884487810ef89
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-01-22 10:05:01 -0300

    Add initial structure Game page

---
+++ src/templates/Game/index.tsx
@@ -0,0 +1,14 @@
+import Base from 'templates/Base'
+import * as S from './styles'
+
+export type GameTemplateProps = {
+  cover: string
+}
+
+const Game = ({ cover }: GameTemplateProps) => (
+  <Base>
+    <S.Cover src={cover} role="img" aria-label="cover" />
+  </Base>
+)
+
+export default Game
