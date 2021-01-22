a4abb86c6c95b4fd2db122b884844c517c345a20
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-01-21 23:37:36 -0300

    Add component Gallery with modal view

--- src/components/Gallery/test.tsx
+++ src/components/Gallery/test.tsx
@@ -1,11 +1,111 @@
 import 'match-media-mock'
-import { screen } from '@testing-library/react'
-
-import Gallery from '.'
+import { fireEvent, screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
+import Gallery from '.'
+import mockItems from './mock'
+
 describe('<Gallery />', () => {
-  it('should render the heading', () => {
-    renderWithTheme(<Gallery />)
+  it('should render thumbnails as buttons', () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
+
+    expect(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i }),
+    ).toHaveAttribute('src', mockItems[0].src)
+
+    expect(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 2/i }),
+    ).toHaveAttribute('src', mockItems[1].src)
+  })
+
+  it('should handle open modal', () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
+
+    // selecionar o nosso modal
+    const modal = screen.getByLabelText('modal')
+
+    // Verificar se o Menu está escondido (hideonmobile)
+    // verificar se o modal tá escondido
+    expect(modal.getAttribute('aria-hidden')).toBe('true')
+    expect(modal).toHaveStyle({
+      opacity: 0,
+      pointerEvents: 'none',
+    })
+
+    // clicar no botão de abrir o menu e verificar se ele abriu
+    fireEvent.click(
+      screen.getByRole('button', {
+        name: /Thumb - Gallery Image 1/i,
+      }),
+    )
+    expect(modal.getAttribute('aria-hidden')).toBe('false')
+    expect(modal).toHaveStyle({
+      opacity: 1,
+    })
+  })
+
+  it('should open modal with selected image', async () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
+
+    // clicar no thumbnail
+    fireEvent.click(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 2/i }),
+    )
+
+    // espero que a imagem da thumbnail seja aberta
+    const img = await screen.findByRole('img', { name: /Gallery Image 2/i })
+    expect(img.parentElement?.parentElement).toHaveClass('slick-active')
+  })
+
+  it('should handle close modal when overlay button cliked', () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
+
+    // selecionar o nosso modal
+    const modal = screen.getByLabelText('modal')
+
+    // clicar no botão de abrir o modal e verificar se ele abriu
+    fireEvent.click(
+      screen.getByRole('button', {
+        name: /Thumb - Gallery Image 1/i,
+      }),
+    )
+    expect(modal.getAttribute('aria-hidden')).toBe('false')
+    expect(modal).toHaveStyle({
+      opacity: 1,
+    })
+
+    fireEvent.click(
+      screen.getByRole('button', {
+        name: /close modal/i,
+      }),
+    )
+
+    expect(modal.getAttribute('aria-hidden')).toBe('true')
+    expect(modal).toHaveStyle({
+      opacity: 0,
+    })
+  })
+
+  it('should handle close modal when ESC cliked', () => {
+    const { container } = renderWithTheme(
+      <Gallery items={mockItems.slice(0, 2)} />,
+    )
+
+    // selecionar o nosso modal
+    const modal = screen.getByLabelText('modal')
+
+    // clicar no botão de abrir o modal e verificar se ele abriu
+    fireEvent.click(
+      screen.getByRole('button', {
+        name: /Thumb - Gallery Image 1/i,
+      }),
+    )
+
+    fireEvent.keyUp(container, { key: 'Escape' })
+
+    expect(modal.getAttribute('aria-hidden')).toBe('true')
+    expect(modal).toHaveStyle({
+      opacity: 0,
+    })
   })
 })
