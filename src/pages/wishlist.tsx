80693ac8484fbd9a014fd85e7c1107230cff182c
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-01-25 11:20:36 -0300

    Create initial wishlist page structure

---
+++ src/pages/wishlist.tsx
@@ -0,0 +1,17 @@
+import Wishlist, { WishlistTemplateProps } from 'templates/Wishlist'
+
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+
+export default function WishlistPage(props: WishlistTemplateProps) {
+  return <Wishlist {...props} />
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      recommendedGames: gamesMock,
+      recommendedHighlight: highlightMock,
+    },
+  }
+}
---
+++ src/templates/Wishlist/index.tsx
@@ -0,0 +1,32 @@
+import { Container } from 'components/Container'
+import { GameCardProps } from 'components/GameCard'
+import Heading from 'components/Heading'
+import { HighlightProps } from 'components/Highlight'
+import Showcase from 'components/Showcase'
+import Base from 'templates/Base'
+import * as S from './styles'
+
+export type WishlistTemplateProps = {
+  recommendedGames: GameCardProps[]
+  recommendedHighlight: HighlightProps
+}
+
+const Wishlist = ({
+  recommendedGames,
+  recommendedHighlight,
+}: WishlistTemplateProps) => (
+  <Base>
+    <Container>
+      <Heading lineLeft lineColor="secondary">
+        Wishlist
+      </Heading>
+    </Container>
+    <Showcase
+      title="You may like this games"
+      games={recommendedGames}
+      highlight={recommendedHighlight}
+    />
+  </Base>
+)
+
+export default Wishlist
