f8f38519c38ac1e7e906d5c720293f33d5bd22a1
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-01-25 11:42:47 -0300

    Add component Grid

---
+++ src/components/Grid/test.tsx
@@ -0,0 +1,22 @@
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import { Grid } from '.'
+
+describe('<Grid />', () => {
+  it('should render corretly', () => {
+    const { container } = renderWithTheme(<Grid />)
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      .c0 {
+        display: grid;
+        grid-template-columns: repeat(auto-fill,minmax(25rem,1fr));
+        margin: 3.2rem 0;
+        grid-gap: 2.4rem;
+      }
+
+      <div
+        class="c0"
+      />
+    `)
+  })
+})
