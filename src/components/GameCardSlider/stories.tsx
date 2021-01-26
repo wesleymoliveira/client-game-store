e2470a5c05d357ae000d3b309cd6f437108655f3
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-01-26 16:33:26 -0300

    add storybook group to game items

--- src/components/GameCardSlider/stories.tsx
+++ src/components/GameCardSlider/stories.tsx
@@ -5,7 +5,7 @@ import GameCardSlider from '.'
 import items from './mock'
 
 export default {
-  title: 'GameCardSlider',
+  title: 'game/GameCardSlider',
   component: GameCardSlider,
   args: { items },
   parameters: {
--- src/components/GameItem/stories.tsx
+++ src/components/GameItem/stories.tsx
@@ -2,7 +2,7 @@ import { Story, Meta } from '@storybook/react/types-6-0'
 import GameItem, { GameItemProps } from '.'
 
 export default {
-  title: 'GameItem',
+  title: 'game/GameItem',
   component: GameItem,
   args: {
     img:
