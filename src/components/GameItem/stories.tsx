9ad7e814cec8c4278eac1243c804240718902fd7
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-01-26 15:23:55 -0300

    Add initial structure GameItem comp

---
+++ src/components/GameItem/stories.tsx
@@ -0,0 +1,23 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import GameItem, { GameItemProps } from '.'
+
+export default {
+  title: 'GameItem',
+  component: GameItem,
+  args: {
+    img:
+      'https://images.unsplash.com/photo-1476820865390-c52aeebb9891?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80',
+    title: 'Red Dead Redemption 2',
+    price: 'R$ 215,00',
+  },
+} as Meta
+
+export const Default: Story<GameItemProps> = (args) => <GameItem {...args} />
+
+export const WithPayment: Story<GameItemProps> = (args) => (
+  <GameItem {...args} />
+)
+
+WithPayment.args = {
+  downloadLink: 'https://link',
+}
