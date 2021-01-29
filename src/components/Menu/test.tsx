941ada2d01a85a773708158c1058981b0c467301
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-01-29 16:52:51 -0300

    Fix Menu test

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -41,7 +41,7 @@ describe('<Menu />', () => {
     expect(screen.queryByText(/My account/i)).not.toBeInTheDocument()
     expect(screen.queryByText(/Wish list/i)).not.toBeInTheDocument()
 
-    expect(screen.getByText(/log in now/i)).toBeInTheDocument()
+    expect(screen.getByText(/sign in now/i)).toBeInTheDocument()
     expect(screen.getByText(/sign up/i)).toBeInTheDocument()
   })
 
