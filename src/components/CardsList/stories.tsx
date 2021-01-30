e4dbda4d2d78e8e702eb9371d5dac789b3540db5
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-01-30 03:49:46 -0300

    Add CardsList

---
+++ src/components/CardsList/stories.tsx
@@ -0,0 +1,14 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import CardsList, { CardsListProps } from '.'
+
+import cardsMock from '../PaymentOptions/mock'
+
+export default {
+  title: 'Profile/CardsList',
+  component: CardsList,
+  args: {
+    cards: cardsMock,
+  },
+} as Meta
+
+export const Default: Story<CardsListProps> = (args) => <CardsList {...args} />
