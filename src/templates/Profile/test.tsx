1a1227ea983bceb41a87549a782d457774aab3d1
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-01-30 04:39:24 -0300

    Add ProfilePage Template structure

---
+++ src/templates/Profile/test.tsx
@@ -0,0 +1,8 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Profile from '.'
+
+describe('<Profile />', () => {
+  it('should render the heading', () => {})
+})
