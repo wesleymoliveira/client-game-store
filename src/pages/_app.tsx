070cdbd39045040b6eab8f49378b114791d2d5ca
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-02-05 23:53:44 -0300

    Add Apollo client with config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,30 @@
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
+import { ThemeProvider } from 'styled-components'
+
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 import GlobalStyles from 'styles/global'
-import { ThemeProvider } from 'styled-components'
 import theme from 'styles/theme'
 
 function App({ Component, pageProps }: AppProps) {
+  const client = new ApolloClient({
+    uri: 'http://localhost:1337/graphql',
+    cache: new InMemoryCache(),
+  })
   return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>Won Games</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="description" content="Your favorite game store" />
-      </Head>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Won Games</title>
+          <link rel="shortcut icon" href="/img/icon-512.png" />
+          <link rel="apple-touch-icon" href="/img/icon-512.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="description" content="Your favorite game store" />
+        </Head>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   )
 }
 
