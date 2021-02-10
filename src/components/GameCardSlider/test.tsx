ec7d672eb6b8e55646ba7ba73e3757fae22424a4
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-02-10 00:24:31 -0300

    Adjustments after script yarn typecheck

--- src/components/GameCardSlider/test.tsx
+++ src/components/GameCardSlider/test.tsx
@@ -3,53 +3,16 @@ import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import GameCardSlider from '.'
-
-const items = [
-  {
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/300x140',
-    price: 'R$ 235,00',
-    promotionalPrice: 'R$ 215,00',
-  },
-  {
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/300x141',
-    price: 'R$ 235,00',
-    promotionalPrice: 'R$ 215,00',
-  },
-  {
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/300x142',
-    price: 'R$ 235,00',
-    promotionalPrice: 'R$ 215,00',
-  },
-  {
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/300x143',
-    price: 'R$ 235,00',
-    promotionalPrice: 'R$ 215,00',
-  },
-  {
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/300x144',
-    price: 'R$ 235,00',
-    promotionalPrice: 'R$ 215,00',
-  },
-]
+import itemsMock from './mock'
 
 describe('<GameSlider />', () => {
   it('should render with 4 active items', () => {
-    const { container } = renderWithTheme(<GameCardSlider items={items} />)
+    const { container } = renderWithTheme(<GameCardSlider items={itemsMock} />)
     expect(container.querySelectorAll('.slick-active')).toHaveLength(4)
   })
 
   it('should render white arrows if color passed', () => {
-    renderWithTheme(<GameCardSlider items={items} color="white" />)
+    renderWithTheme(<GameCardSlider items={itemsMock} color="white" />)
 
     expect(screen.getByLabelText(/previous games/i)).toHaveStyle({
       color: '#FAFAFA',
