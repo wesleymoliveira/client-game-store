cd69c2e4ada377607e301da2fe1e571815dc25dc
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-02-10 10:31:23 -0300

    Add QueryHome to add dynamic banners

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,37 @@
+import { initializeApollo } from 'utils/apollo'
 import Home, { HomeTemplateProps } from 'templates/Home'
 import bannersMock from 'components/BannerSlider/mock'
 import gamesMock from 'components/GameCardSlider/mock'
 import HighlightMock from 'components/Highlight/mock'
+import { QueryHome } from 'graphql/generated/QueryHome'
+import { QUERY_HOME } from 'graphql/queries/home'
 
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<QueryHome>({ query: QUERY_HOME })
+
   return {
     props: {
-      banners: bannersMock,
+      revalidate: 60,
+      banners: data.banners.map((banner) => ({
+        img: banner.image?.url
+          ? `http://localhost:1337${banner.image?.url}`
+          : `http://localhost:1337/uploads/nothing.jpg`,
+        title: banner.title,
+        subtitle: banner.subtitle,
+        buttonLabel: banner.button?.label,
+        buttonLink: banner.button?.link,
+        ...(banner.ribbon && {
+          ribbon: banner.ribbon.text,
+          ribbonColor: banner.ribbon.color,
+          ribbonSize: banner.ribbon.sizes,
+        }),
+      })),
       newGames: gamesMock,
       mostPopularHighlight: HighlightMock,
       mostPopularGames: gamesMock,
