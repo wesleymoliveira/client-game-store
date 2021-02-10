3a807d4f25bc072eb96a5411210d60825d7f1ad2
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-02-09 23:54:19 -0300

    Remove apollo config from tests

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
     '!src/**/stories.tsx',
     '!src/pages/**/*.tsx',
     '!src/styles/*.ts',
+    '!src/utils/apollo.ts',
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/', '<rootDir>/.jest/'],
