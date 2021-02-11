2d74c8e8252bd89cb7b1b3a86ee748b6610a8684
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-02-11 15:38:37 -0300

    Change highlight components to use dynamic data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { initializeApollo } from 'utils/apollo'
 import Home, { HomeTemplateProps } from 'templates/Home'
 
-import HighlightMock from 'components/Highlight/mock'
 import { QueryHome } from 'graphql/generated/QueryHome'
 import { QUERY_HOME } from 'graphql/queries/home'
 
@@ -46,7 +45,17 @@ export async function getStaticProps() {
       })),
 
       mostPopularGamesTitle: sections?.popularGames?.title,
-      mostPopularHighlight: HighlightMock,
+      mostPopularHighlight: {
+        title: sections?.popularGames?.highlight?.title,
+        subtitle: sections?.popularGames?.highlight?.subtitle,
+        backgroundImage: `http://localhost:1337${sections?.popularGames?.highlight?.background?.url}`,
+        buttonLabel: sections?.popularGames?.highlight?.buttomLabel,
+        buttonLink: sections?.popularGames?.highlight?.buttonLink,
+        ...(sections?.popularGames?.highlight?.floatImage && {
+          floatImage: `http://localhost:1337${sections?.popularGames?.highlight?.floatImage.url}`,
+        }),
+        alignment: sections?.popularGames?.highlight?.aligment,
+      },
       mostPopularGames: sections!.popularGames!.games.map((game) => ({
         title: game.name,
         slug: game.slug,
@@ -67,7 +76,17 @@ export async function getStaticProps() {
           : `http://localhost:1337/uploads/nothing.jpg`,
         price: game.price,
       })),
-      upcomingHighlight: HighlightMock,
+      upcomingHighlight: {
+        title: sections?.upComingGames?.highlight?.title,
+        subtitle: sections?.upComingGames?.highlight?.subtitle,
+        backgroundImage: `http://localhost:1337${sections?.upComingGames?.highlight?.background?.url}`,
+        buttonLabel: sections?.upComingGames?.highlight?.buttomLabel,
+        buttonLink: sections?.upComingGames?.highlight?.buttonLink,
+        ...(sections?.upComingGames?.highlight?.floatImage && {
+          floatImage: `http://localhost:1337${sections?.upComingGames?.highlight?.floatImage.url}`,
+        }),
+        alignment: sections?.upComingGames?.highlight?.aligment,
+      },
 
       freeGamesTitle: sections?.freeGames?.title,
       freeGames: freeGames.map((game) => ({
@@ -79,7 +98,17 @@ export async function getStaticProps() {
           : `http://localhost:1337/uploads/nothing.jpg`,
         price: game.price,
       })),
-      freeHighlight: HighlightMock,
+      freeHighlight: {
+        title: sections?.freeGames?.highlight?.title,
+        subtitle: sections?.freeGames?.highlight?.subtitle,
+        backgroundImage: `http://localhost:1337${sections?.freeGames?.highlight?.background?.url}`,
+        buttonLabel: sections?.freeGames?.highlight?.buttomLabel,
+        buttonLink: sections?.freeGames?.highlight?.buttonLink,
+        ...(sections?.freeGames?.highlight?.floatImage && {
+          floatImage: `http://localhost:1337${sections?.freeGames?.highlight?.floatImage.url}`,
+        }),
+        alignment: sections?.freeGames?.highlight?.aligment,
+      },
     },
   }
 }
