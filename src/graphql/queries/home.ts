f9bd9f7ea80f5088ff8b01b689a38f317799eb73
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-02-12 14:11:38 -0300

    Change Home querie to use dynamic date

--- src/graphql/queries/home.ts
+++ src/graphql/queries/home.ts
@@ -4,12 +4,12 @@ import { GameFragment } from 'graphql/fragments/game'
 import { HighlightFragment } from 'graphql/fragments/highlight'
 
 export const QUERY_HOME = gql`
-  query QueryHome {
+  query QueryHome($date: Date!) {
     banners {
       ...BannerFragment
     }
     newGames: games(
-      where: { release_date_lte: "2021-01-27" }
+      where: { release_date_lte: $date }
       sort: "release_date:desc"
       limit: 8
     ) {
@@ -17,7 +17,7 @@ export const QUERY_HOME = gql`
     }
 
     upcommingGames: games(
-      where: { release_date_gt: "2021-02-01" }
+      where: { release_date_gt: $date }
       sort: "release_date:asc"
       limit: 8
     ) {
