21867aa4dca5c2a1d020e9643c574ab2f5df85e1
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-02-17 12:04:11 -0300

    Add recommendedGames to Cart Page

--- src/templates/Cart/index.tsx
+++ src/templates/Cart/index.tsx
@@ -13,12 +13,14 @@ import PaymentOptions, { PaymentOptionsProps } from 'components/PaymentOptions'
 import Empty from 'components/Empty'
 
 export type CartProps = {
+  recommendedTitle: string
   recommendedGames: GameCardProps[]
   recommendedHighlight: HighlightProps
 } & CartListProps &
   Pick<PaymentOptionsProps, 'cards'>
 
 const Cart = ({
+  recommendedTitle,
   recommendedGames,
   recommendedHighlight,
   items = [],
@@ -50,7 +52,7 @@ const Cart = ({
       </Container>
 
       <Showcase
-        title="You may like this games"
+        title={recommendedTitle || 'You may like this games'}
         games={recommendedGames}
         highlight={recommendedHighlight}
       />
