a76efaf77f24f07b9ef796b3ea221e04af4dc98f
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-02-17 21:34:38 -0300

    Add Loading Spinner

--- src/templates/Games/index.tsx
+++ src/templates/Games/index.tsx
@@ -1,4 +1,5 @@
 import Base from 'templates/Base'
+import Spinner from 'react-spinner-material'
 import { KeyboardArrowDown as ArrowDown } from '@styled-icons/material-outlined/KeyboardArrowDown'
 import ExploreSideBar, { ItemProps } from 'components/ExploreSideBar'
 import GameCard, { GameCardProps } from 'components/GameCard'
@@ -7,6 +8,7 @@ import * as S from './styles'
 import { useQuery } from '@apollo/client'
 import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
 import { QUERY_GAMES } from 'graphql/queries/games'
+import theme from 'styles/theme'
 
 export type GamesTemplateProps = {
   games?: GameCardProps[]
@@ -31,24 +33,40 @@ const GamesTemplate = ({ filterItems }: GamesTemplateProps) => {
     <Base>
       <S.Main>
         <ExploreSideBar onFilter={handleFilter} items={filterItems} />
-        <section>
-          <Grid>
-            {data?.games.map((game) => (
-              <GameCard
-                key={game.slug}
-                title={game.name}
-                slug={game.slug}
-                developer={game.developers[0].name}
-                img={`http://localhost:1337${game.cover!.url}`}
-                price={game.price}
-              />
-            ))}
-          </Grid>
-          <S.ShowMore role="button" onClick={handleShowMore}>
-            <p>Show More</p>
-            <ArrowDown size={35} />
-          </S.ShowMore>
-        </section>
+
+        {/* Na prática esse loading nunca vai ser chamado,
+            a função initialApolloState alimenta os dados
+            do frontend com a requisição para o backend  - SEO
+        */}
+        {loading ? (
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <Spinner
+              radius={120}
+              color={theme.colors.primary}
+              stroke={2}
+              visible={true}
+            />
+          </div>
+        ) : (
+          <section>
+            <Grid>
+              {data?.games.map((game) => (
+                <GameCard
+                  key={game.slug}
+                  title={game.name}
+                  slug={game.slug}
+                  developer={game.developers[0].name}
+                  img={`http://localhost:1337${game.cover!.url}`}
+                  price={game.price}
+                />
+              ))}
+            </Grid>
+            <S.ShowMore role="button" onClick={handleShowMore}>
+              <p>Show More</p>
+              <ArrowDown size={35} />
+            </S.ShowMore>
+          </section>
+        )}
       </S.Main>
     </Base>
   )
