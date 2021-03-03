b0e4b7fc0bccc41189ef7b5a854f1b0350fb00f2
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-03-03 11:14:41 -0300

    Add Empty when don't find games

--- src/templates/Games/index.tsx
+++ src/templates/Games/index.tsx
@@ -13,6 +13,7 @@ import GameCard from 'components/GameCard'
 import { Grid } from 'components/Grid'
 import * as S from './styles'
 import theme from 'styles/theme'
+import Empty from 'components/Empty'
 
 export type GamesTemplateProps = {
   filterItems: ItemProps[]
@@ -68,22 +69,31 @@ const GamesTemplate = ({ filterItems }: GamesTemplateProps) => {
           </div>
         ) : (
           <section>
-            <Grid>
-              {data?.games.map((game) => (
-                <GameCard
-                  key={game.slug}
-                  title={game.name}
-                  slug={game.slug}
-                  developer={game.developers[0].name}
-                  img={`http://localhost:1337${game.cover!.url}`}
-                  price={game.price}
-                />
-              ))}
-            </Grid>
-            <S.ShowMore role="button" onClick={handleShowMore}>
-              <p>Show More</p>
-              <ArrowDown size={35} />
-            </S.ShowMore>
+            {data?.games.length ? (
+              <>
+                <Grid>
+                  {data?.games.map((game) => (
+                    <GameCard
+                      key={game.slug}
+                      title={game.name}
+                      slug={game.slug}
+                      developer={game.developers[0].name}
+                      img={`http://localhost:1337${game.cover!.url}`}
+                      price={game.price}
+                    />
+                  ))}
+                </Grid>
+                <S.ShowMore role="button" onClick={handleShowMore}>
+                  <p>Show More</p>
+                  <ArrowDown size={35} />
+                </S.ShowMore>
+              </>
+            ) : (
+              <Empty
+                title=":("
+                description="We didn't find any games with this filter"
+              />
+            )}
           </section>
         )}
       </S.Main>
