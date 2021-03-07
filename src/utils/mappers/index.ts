89a2e5ed37afe3ad58dc9931a17cb647c74fad59
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-03-06 23:51:14 -0300

    Add mappers test

--- src/utils/mappers/index.ts
+++ src/utils/mappers/index.ts
@@ -22,36 +22,30 @@ export const bannerMapper = (banners: QueryHome_banners[]) => {
 }
 
 export const gamesMapper = (games: QueryGames_games[] | null | undefined) => {
-  return (
-    games &&
-    games.map((game) => ({
-      title: game.name,
-      slug: game.slug,
-      developer: game.developers[0].name,
-      img: game.cover?.url
-        ? `http://localhost:1337${game.cover?.url}`
-        : `http://localhost:1337/uploads/nothing.jpg`,
-      price: game.price,
-    }))
-  )
+  return games
+    ? games.map((game) => ({
+        id: game.id,
+        title: game.name,
+        slug: game.slug,
+        developer: game.developers[0].name,
+        img: `http://localhost:1337${game.cover?.url}`,
+        price: game.price,
+      }))
+    : []
 }
 
 export const highlightMapper = (
   highlight: QueryHome_sections_freeGames_highlight | null | undefined,
 ) => {
-  return (
-    highlight && {
-      title: highlight.title,
-      subtitle: highlight.subtitle,
-      ...(highlight.background && {
-        backgroundImage: `http://localhost:1337${highlight.background.url}`,
-      }),
-      buttonLabel: highlight.buttomLabel,
-      buttonLink: highlight.buttonLink,
-      ...(highlight.floatImage && {
-        floatImage: `http://localhost:1337${highlight.floatImage.url}`,
-      }),
-      alignment: highlight.aligment,
-    }
-  )
+  return highlight
+    ? {
+        title: highlight.title,
+        subtitle: highlight.subtitle,
+        backgroundImage: `http://localhost:1337${highlight.background?.url}`,
+        floatImage: `http://localhost:1337${highlight.floatImage?.url}`,
+        buttonLabel: highlight.buttomLabel,
+        buttonLink: highlight.buttonLink,
+        alignment: highlight.aligment,
+      }
+    : {}
 }
