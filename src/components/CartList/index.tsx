b661ffec7bd31921d197b5cc1d2d51249a6a1684
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-03-10 13:20:33 -0300

    Add Context to CartList

--- src/components/CartList/index.tsx
+++ src/components/CartList/index.tsx
@@ -1,44 +1,47 @@
 import Button from 'components/Button'
 import Empty from 'components/Empty'
-import GameItem, { GameItemProps } from 'components/GameItem'
+import GameItem from 'components/GameItem'
+import { useCart } from 'hooks/use-cart'
 import Link from 'next/link'
 import * as S from './styles'
 
 export type CartListProps = {
-  items?: GameItemProps[]
-  total?: string
   hasButton?: boolean
 }
 
-const CartList = ({ items = [], total, hasButton = false }: CartListProps) => (
-  <S.Wrapper isEmpty={!items.length}>
-    {items.length ? (
-      <>
-        {items.map((item) => (
-          <GameItem key={item.title} {...item} />
-        ))}
-        <S.Footer>
-          {!hasButton && <span>Total</span>}
-          <S.Total>{total}</S.Total>
-          {hasButton && (
-            <Link href="/cart">
-              <Button as="a" size="small">
-                Buy it now
-              </Button>
-            </Link>
-          )}
-        </S.Footer>
-      </>
-    ) : (
-      <>
-        <Empty
-          title="Your cart is empty"
-          description="Please return and experiment new offers"
-          hasLink
-        />
-      </>
-    )}
-  </S.Wrapper>
-)
+const CartList = ({ hasButton = false }: CartListProps) => {
+  const { items, total } = useCart()
+
+  return (
+    <S.Wrapper isEmpty={!items.length}>
+      {items.length ? (
+        <>
+          {items.map((item) => (
+            <GameItem key={item.title} {...item} />
+          ))}
+          <S.Footer>
+            {!hasButton && <span>Total</span>}
+            <S.Total>{total}</S.Total>
+            {hasButton && (
+              <Link href="/cart">
+                <Button as="a" size="small">
+                  Buy it now
+                </Button>
+              </Link>
+            )}
+          </S.Footer>
+        </>
+      ) : (
+        <>
+          <Empty
+            title="Your cart is empty"
+            description="Please return and experiment new offers"
+            hasLink
+          />
+        </>
+      )}
+    </S.Wrapper>
+  )
+}
 
 export default CartList
--- src/components/CartList/stories.tsx
+++ src/components/CartList/stories.tsx
@@ -1,14 +1,18 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
 import CartList, { CartListProps } from '.'
 
-import mockItems from './mock'
+import items from './mock'
 
 export default {
   title: 'CartList',
   component: CartList,
-  args: {
-    items: mockItems,
-    total: 'R$ 215,00',
+  argTypes: {
+    cartContextValue: {
+      type: '',
+    },
+    items: {
+      type: '',
+    },
   },
   parameters: {
     backgrounds: {
@@ -17,17 +21,27 @@ export default {
   },
 } as Meta
 
-export const Default: Story<CartListProps> = (args) => (
+export const Default: Story = (args) => (
   <div style={{ maxWidth: 800 }}>
     <CartList {...args} />
   </div>
 )
 
-export const WithButton: Story<CartListProps> = (args) => (
+Default.args = {
+  total: 'R$ 330,00',
+  cartContextValue: { items },
+}
+
+export const WithButton: Story = (args) => (
   <div style={{ maxWidth: 800 }}>
-    <CartList hasButton {...args} />
+    <CartList {...args} hasButton />
   </div>
 )
+
+WithButton.args = {
+  total: 'R$ 330,00',
+  cartContextValue: { items },
+}
 
 export const Empty: Story<CartListProps> = () => (
   <div style={{ maxWidth: 800 }}>
