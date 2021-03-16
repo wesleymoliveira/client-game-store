80cf37dbfc6d9e5133117612ec4722b3bda9f129
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-03-16 04:21:47 -0300

    Add loading to CartList

--- src/components/CartList/test.tsx
+++ src/components/CartList/test.tsx
@@ -35,4 +35,15 @@ describe('<CartList />', () => {
 
     expect(screen.getByText(/your cart is empty/i)).toBeInTheDocument()
   })
+
+  it('should render loading', () => {
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      loading: true,
+    }
+
+    render(<CartList hasButton />, { cartProviderProps })
+
+    expect(screen.getByTitle(/loading/i)).toBeInTheDocument()
+  })
 })
