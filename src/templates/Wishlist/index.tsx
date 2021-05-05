8534ce4182abc13f300f98c6ceca09c9cf6e00b5
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-05-05 10:55:55 -0300

    Add Apollo cache Info and Query to wishlist page

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -5,48 +5,55 @@ import GameCard, { GameCardProps } from 'components/GameCard'
 import { Grid } from 'components/Grid'
 import Heading from 'components/Heading'
 import { HighlightProps } from 'components/Highlight'
+import Loader from 'components/Loader'
 import Showcase from 'components/Showcase'
+import { useWishlist } from 'hooks/use-wishlist'
 import Base from 'templates/Base'
+import * as S from './styles'
 
 export type WishlistTemplateProps = {
-  games?: GameCardProps[]
   recommendedTitle?: string
   recommendedGames: GameCardProps[]
   recommendedHighlight: HighlightProps
 }
 
 const Wishlist = ({
-  games = [],
   recommendedTitle,
   recommendedGames,
   recommendedHighlight,
-}: WishlistTemplateProps) => (
-  <Base>
-    <Container>
-      <Heading lineLeft lineColor="secondary">
-        Wishlist
-      </Heading>
-      {games.length >= 1 ? (
-        <Grid>
-          {games?.map((game, index) => (
-            <GameCard key={`wishlist-${index}`} {...game} />
-          ))}
-        </Grid>
-      ) : (
-        <Empty
-          hasLink
-          title="Your wishlist is Empty"
-          description="Games added to your wishlist will appear here"
-        />
-      )}
-      <Divider />
-    </Container>
-    <Showcase
-      title={recommendedTitle || 'You may like this games'}
-      games={recommendedGames}
-      highlight={recommendedHighlight}
-    />
-  </Base>
-)
-
+}: WishlistTemplateProps) => {
+  const { items, loading } = useWishlist()
+  return (
+    <Base>
+      <Container>
+        <Heading lineLeft lineColor="secondary">
+          Wishlist
+        </Heading>
+        {loading ? (
+          <S.Loading>
+            <Loader />
+          </S.Loading>
+        ) : items.length >= 1 ? (
+          <Grid>
+            {items?.map((game, index) => (
+              <GameCard key={`wishlist-${index}`} {...game} />
+            ))}
+          </Grid>
+        ) : (
+          <Empty
+            hasLink
+            title="Your wishlist is Empty"
+            description="Games added to your wishlist will appear here"
+          />
+        )}
+        <Divider />
+      </Container>
+      <Showcase
+        title={recommendedTitle || 'You may like this games'}
+        games={recommendedGames}
+        highlight={recommendedHighlight}
+      />
+    </Base>
+  )
+}
 export default Wishlist
